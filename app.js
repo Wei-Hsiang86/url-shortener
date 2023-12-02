7e3b84d04846e8cf5f212f8fcae28b6d617f0a4d
Wei-Hsiang86 <frankflyhuang@gmail.com>
2023-12-02 17:54:49 +0800

    build: route

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+//// 設定路由
 app.get("/", (req, res) => {
-  res.send("RRRRRRRRRRRRR");
+  res.redirect("/movies");
+});
+
+app.get("/movies", (req, res) => {
+  res.send("listing movies");
+});
+
+app.get("/movie/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`read movie: ${id}`);
 });
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
 });
+
+//// 功能
